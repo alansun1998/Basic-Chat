9993eb03810e1102592ff01f3b0057519c83aac8
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2019-12-03 11:56:18 -0600

    Reworked private chat system, needs help

--- src/assignment7/Profile.java
+++ src/assignment7/Profile.java
@@ -7,19 +7,15 @@ import java.util.ArrayList;
 public class Profile {
     String username;
     Socket sock;
-    public static ArrayList<Profile> friends = new ArrayList<Profile>();
 
-    public Profile(String name){
-        this.username = name;
-    }
+//    public Profile(String name) {
+//        this.username = name;
+//    }
 
     public Profile(String name, Socket psock) throws IOException {
         this.username = name;
         this.sock = psock;
-        ServerMain.addUsers(this);
-    }
-
-    public void addFriend(Profile fren){
-        this.friends.add(fren);
+//        ServerMain.addUsers(this);
+//        System.out.println("added user "+name+" (Total users: "+ServerMain.getUsers().size()+")");
     }
 }
