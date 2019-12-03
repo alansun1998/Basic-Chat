e713f29621d7b6fcf7fb4f61162dd9f78cd06864
alansun1998 <alansun0@gmail.com>
2019-12-03 01:59:42 -0600

    made it look a bit better

--- src/assignment7/ChatRoom.java
+++ src/assignment7/ChatRoom.java
@@ -1,13 +1,19 @@
 package assignment7;
 
+import javafx.beans.value.ObservableValueBase;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -29,6 +35,9 @@ public class ChatRoom extends Stage implements Observer {
     public ChatRoom(){
         GridPane chat_Pane = new GridPane();
         chat_Pane.setPadding(new Insets(5));
+        GridPane menu = new GridPane();
+        menu.setPadding(new Insets(5));
+        
         try{
             connect(ClientMain.ip);
         }catch(Exception excep) {}
@@ -48,7 +57,6 @@ public class ChatRoom extends Stage implements Observer {
                 writer.flush();
                 outgoing.setText("");;
                 outgoing.requestFocus();
-
             }
         });
 
@@ -59,12 +67,36 @@ public class ChatRoom extends Stage implements Observer {
                 new newChatCreate(ClientMain.currentUser);
             }
         });
+        ChoiceBox<String> userList = new ChoiceBox<String>();
+        try{
+            for(Profile c: ServerMain.getUsers()){
+                userList.getItems().add(c.username);
+            }
+        }catch (NullPointerException e){
+            userList.getItems().add("There are no users :(");
+        }
+        if(ServerMain.getUsers().size() == 1)
+            userList.getItems().add("There are no users :(");
+        Button addFriend = new Button("Add Friend");
+        newChat.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ClientMain.currentUser.addFriend(ServerMain.findUser(userList.getValue()));
+            }
+        });
+
 
 
         chat_Pane.add(hist, 0, 0);
         chat_Pane.add(outgoing,0,1);
-        chat_Pane.add(send,1,1);
-        chat_Pane.add(newChat,1,0);
+        menu.add(send,0,1);
+        menu.add(newChat,0,2);
+        menu.add(addFriend,1,0);
+        menu.add(userList,0,0);
+        menu.setAlignment(Pos.CENTER);
+        chat_Pane.add(menu,1,1);
+        hist.setFitToWidth(true);
+        chat_Pane.setConstraints(hist,0,0,2,1);
         this.setScene(new Scene(chat_Pane));
         this.show();
     }
