358d62fdc72ef22a06dae222a63f3a09f2b2cc84
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2019-12-03 11:57:51 -0600

    Reworked private chat system, needs help

--- src/assignment7/newChatCreate.java
+++ src/assignment7/newChatCreate.java
@@ -1,33 +1,32 @@
 package assignment7;
 
+import java.util.ArrayList;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class newChatCreate extends Stage {
 
     Stage newChat = new Stage();
     public newChatCreate(Profile currentUser){
-        Pane friend_Pane = new HBox(5);
-        friend_Pane.setPadding(new Insets(5));
+        Pane privateChat_Pane = new VBox(5);
+        privateChat_Pane.setPadding(new Insets(5));
         Label addFriend = new Label("Add person to chat: ");
-        ChoiceBox<String> friendList = new ChoiceBox<String>();
+        ArrayList<CheckBox> chooseChat = new ArrayList<CheckBox>();
         try{
-            for(Profile c: currentUser.friends){
-                friendList.getItems().add(c.username);
+            for(Profile c: ServerMain.users){
+                chooseChat.add(new CheckBox(c.username));
             }
         }catch (NullPointerException e){
-            friendList.getItems().add("You have no friends :(");
+        	CheckBox filler = new CheckBox("no one's here lol :P");
+        	filler.setDisable(true);
+        	chooseChat.add(filler);
         }
-        if(currentUser.friends.size() == 0)
-            friendList.getItems().add("You have no friends :(");
         Button add = new Button("Add");
         add.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -36,9 +35,9 @@ public class newChatCreate extends Stage {
                 newChat.close();
             }
         });
-        friend_Pane.getChildren().addAll(addFriend,friendList,add);
+        privateChat_Pane.getChildren().addAll(addFriend,add); privateChat_Pane.getChildren().addAll(chooseChat);
 
-        newChat.setScene(new Scene(friend_Pane));
+        newChat.setScene(new Scene(privateChat_Pane));
         newChat.show();
     }
 }
