8f28407eb65a18a7046741401f82246862e2bd0d
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2019-12-03 19:53:07 -0600

    Changed profile to be observer(?)

--- src/assignment7/Profile.java
+++ src/assignment7/Profile.java
@@ -3,10 +3,17 @@ package assignment7;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
-public class Profile {
+import javafx.scene.control.TextArea;
+
+public class Profile implements Observer
+{
     String username;
     Socket sock;
+    TextArea chat_feed = new TextArea();
+    ArrayList<Profile> otherUsers = new ArrayList<Profile>();
 
 //    public Profile(String name) {
 //        this.username = name;
@@ -15,12 +22,21 @@ public class Profile {
     public Profile(String name, Socket psock) throws IOException {
         this.username = name;
         this.sock = psock;
-        ServerMain.addUsers(this);
-        System.out.println("added user "+name+" (Total users: "+ServerMain.getUsers().size()+")");
+//        System.out.println("added user "+name+" (Total users: "+ServerMain.getUsers().size()+")");
     }
 
     @Override
     public String toString(){
         return username;
     }
+
+	@Override
+	public void update(Observable o, Object arg)
+	{
+		if(arg.getClass().equals(String.class)) {chat_feed.appendText(arg.toString());}
+		else if(arg.getClass().equals(Profile.class))
+		{
+			if(!(otherUsers.contains(arg))) {otherUsers.add((Profile) arg);}
+		}
+	}
 }
